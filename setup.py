467dad661f5b04638317c3773e0bed8ccf8534a5
Marie-Caroline <marie-caroline.schmied@chartes.psl.eu>
2019-03-31 13:13:29 +0200

    ajout de command_line dans le setup

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@
 
 from setuptools import setup, find_packages
 
+
 setup(
-    name="Devoir_pyrrha",
-    version="1.0",
-    packages=find_packages(),
-    description="correcteur d'annotations pyrrha",
-    author="Marie-Caroline Schmied et Emilie Blotière",
-    author_email="marie-caroline.schmied@chartes.psl.eu , emilie.blotiere@chartes.psl.eu",
-    install_requires=['Click'],
-    entry_points = {
-        'console_scripts': [
-            'test= Devoir_pyrrha.code:test',
-        ],
-    },
+      name="pyrrha_ci",
+      version="0.1",
+      author_email="username@email.address",
+      packages=['pyrrha_ci'],
+      package_data={},
+      description="correcteur d'annotations pyrrha",
+      author="Marie-Caroline Schmied et Emilie Blotière",
+      author_email="marie-caroline.schmied@chartes.psl.eu , emilie.blotiere@chartes.psl.eu",
+      install_requires=['pyyaml', 'click'],
+      entry_points={
+        'console_scripts': ['pyrrha_ci = pyrrha_ci.code:test']
+      }
 )
