817366619330a0d48711bb610f74ae54eea54ddd
Marie-Caroline <marie-caroline.schmied@chartes.psl.eu>
2019-03-31 14:16:47 +0200

    Deux fois adresses mail

--- setup.py
+++ setup.py
@@ -6,12 +6,11 @@ from setuptools import setup, find_packages
 setup(
       name="pyrrha_ci",
       version="0.1",
-      author_email="username@email.address",
+      author_email="marie-caroline.schmied@chartes.psl.eu , emilie.blotiere@chartes.psl.eu",
       packages=['pyrrha_ci'],
       package_data={},
       description="correcteur d'annotations pyrrha",
       author="Marie-Caroline Schmied et Emilie Blotière",
-      author_email="marie-caroline.schmied@chartes.psl.eu , emilie.blotiere@chartes.psl.eu",
       install_requires=['pyyaml', 'click'],
       entry_points={
         'console_scripts': ['pyrrha_ci = pyrrha_ci.code:test']
