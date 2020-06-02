cc77354caa0647e87cd9d2d1bc5736e5c38b252d
Thibault Clérice <leponteineptique@gmail.com>
2020-06-02 10:01:17 +0200

    (CI) Fix for wrong requirements

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
       name="pyrrha_ci",
-      version="0.1",
-      author_email="marie-caroline.schmied@chartes.psl.eu , emilie.blotiere@chartes.psl.eu",
+      version="0.0.1",
+      author_email="marie-caroline.schmied@chartes.psl.eu , emilie.blotiere@chartes.psl.eu, thibault.clerice@chartes.psl.eu",
       packages=['pyrrha_ci'],
       package_data={},
       description="correcteur d'annotations pyrrha",
-      author="Marie-Caroline Schmied et Emilie Blotière",
-      install_requires=['pyyaml', 'click'],
+      author="Marie-Caroline Schmied, Emilie Blotière, Thibault Clérice",
+      install_requires=['pyyaml', 'click', 'regex'],
       entry_points={
         'console_scripts': ['pyrrha-ci = pyrrha_ci.code:test']
       }
